5dd1a1285e9ad3b1f78f2921a20443f7eb218014
Justin <justin.guida@snhu.edu>
2025-09-21 23:29:17 -0400

    Update formula to v1.0.0

--- Formula/linked-list.rb
+++ Formula/linked-list.rb
@@ -1,6 +1,9 @@
 class LinkedList < Formula
   desc "C++ linked list demo with CSV loader"
   homepage "https://github.com/jguida941/LinkedList"
+  url "https://github.com/jguida941/LinkedList/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "c73a2851467e391b8fcc81548fc3ec72a2ec1b2741572892659a969e9fd2cd58"
+  version "1.0.0"
   head "https://github.com/jguida941/LinkedList.git", branch: "main"
 
   depends_on "cmake" => :build
